e7588ceaef6c2d8329f99750d3e16002c1691658
RevKraft <ramiro.velez.koeppel@gmail.com>
2024-06-24 12:04:16 -0500

    Error in signup component

--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -24,7 +24,7 @@ import { type UserRegister, UsersService } from "../client"
 import type { ApiError } from "../client/core/ApiError"
 import useCustomToast from "../hooks/useCustomToast"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { useNavigate } from 'react-router-dom'
+//import { useNavigate } from 'react-router-dom'
 
 
 interface UserRegisterForm extends UserRegister {
@@ -45,7 +45,6 @@ export const Route = createFileRoute("/signup")({
 function Signup() {
   const queryClient = useQueryClient()
   const showToast = useCustomToast()
-  const navigate = useNavigate()
   const {
     register,
     handleSubmit,
@@ -68,12 +67,14 @@ function Signup() {
     mutationFn: (data: UserRegister) =>
       UsersService.registerUser({ requestBody: data }),
     onSuccess: () => {
+      //const navigate = useNavigate()
       showToast("Success!", "User created successfully.", "success")
       reset()
-      navigate("/login")
+      //navigate('dev.12cashs.com/login'); // Redirect to new page
+      //navigate("/login")
     },
     onError: (err: ApiError) => {
-      const errDetail = (err.body as any)?.detail 
+      const errDetail = (err.body as any)?.detail
       showToast("Something went wrong.", `${errDetail}`, "error")
     },
     onSettled: () => {
