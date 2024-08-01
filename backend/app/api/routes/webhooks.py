0fa86cb7b0cbd1a4be836e409bfa7b3fae587048
RevKraft <ramiro.velez.koeppel@gmail.com>
2024-08-01 15:11:42 +0000

    Lint fix

--- backend/app/api/routes/webhooks.py
+++ backend/app/api/routes/webhooks.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlmodel import col, delete, func, select
+from typing import Any, Dict
 
 from app import crud
 from app.api.deps import (
@@ -32,7 +33,7 @@ from app.utils import generate_new_account_email, send_email
 router = APIRouter()
 
 @router.post("/nium", response_model=None)
-async def event_webhook(request: Request, body: RequestBodyModel) -> Any:
+async def event_webhook(request: Request, body: RequestBodyModel) -> Dict[str, Any]:
     headers = dict(request.headers)
-    body = body.root
-    return {"headers": headers, "body": body, "message": "Event processed"}
+    body_content = body.root
+    return {"headers": headers, "body": body_content, "message": "Event processed"}
